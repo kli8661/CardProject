4d832cf78d288209d44dd5f0d620ce6893ae3c55
BT_1N3_06 <kli8661@bths.edu>
2018-12-10 09:56:20 -0500

    Challenge 1

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,40 @@
 public class Card {
+
+    private String rank;
+    private String suit;
+    private int pointValue;
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public Card(String rank, String suit, int pointValue)
+    {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
+
+    public boolean equals(Card otherCard)
+    {
+        if(this.rank.equals(otherCard.rank) && this.suit.equals(otherCard.suit) && this.pointValue == (otherCard.pointValue))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public String toString()
+    {
+        return this.suit + " of " + pointValue;
+    }
 }
--- src/DeckTester.java
+++ src/DeckTester.java
@@ -1,2 +1,6 @@
 public class DeckTester {
+    public static void main(String[] args)
+    {
+
+    }
 }
